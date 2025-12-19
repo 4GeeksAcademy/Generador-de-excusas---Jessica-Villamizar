ac21bb2c4c267afafc0f39fb3944b1576599a6ec
1989212 <jessica_villamizar.c@hotmail.com>
2025-12-19 14:50:57 +0000

    proyecto terminado

--- src/app.js
+++ src/app.js
@@ -8,4 +8,25 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  let who = ['El perro', 'Mi abuela ', 'El cartero', 'Mi pajaro', 'Loco'];
+
+  let action = ['se comio', 'orino', 'choco', 'rompio', ''];
+
+  let what = ['mi tarea', 'mi telefono', 'el coche'];
+
+  let when = ['antes de clases', 'mientras dormia', 'mientras hacia ejercicio', 'durante el almuerzo', 'mientras rezaba'];
+
+    //let whoAleatorio = seleccionarAleatorio(who);
+    //let actionAleatorio = seleccionarAleatorio(action);
+    //let whatAleatorio = seleccionarAleatorio(what);
+    //let whenAleatorio = seleccionarAleatorio(when);
+  let excuseElement = this.document.querySelector("#excuse");
+  
+  excuseElement.innerHTML = `${seleccionarAleatorio(who)} ${seleccionarAleatorio(action)} ${seleccionarAleatorio(what)} ${seleccionarAleatorio(when)}`
+};
+
+function seleccionarAleatorio(lista) {
+  let elemetoAleatorio = Math.floor(Math.random() * lista.length);
+  return lista[elemetoAleatorio];  
 };
